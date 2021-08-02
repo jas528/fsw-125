db527eb73e0efcdeae1be83ddc4c1426bec0b791
jas528 <76809910+jas528@users.noreply.github.com>
2021-08-02 14:23:58 -0500

    add

---
+++ week1/math/app.js
@@ -0,0 +1,11 @@
+const {sum,sub,div,multiply}= require('./math');
+
+
+
+console.log(sum(10,2));
+
+console.log(sub(10,2));
+
+console.log(div(10,2));
+
+console.log(multiply(10,2));
---
+++ week1/math/math.js
@@ -0,0 +1,16 @@
+function sum (a,b){
+    return a+b
+};
+//module.exports=sum;
+ 
+function sub(a,b){
+    return a-b
+};
+function div (a,b){
+return a/b
+};
+ function multiply (a,b){
+return a*b
+ };
+
+module.exports={sub,sum,div,multiply};
